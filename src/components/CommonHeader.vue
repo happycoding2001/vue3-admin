a5ef21d5892580b5c72d3ab50b2f1d3d42b3b97a
dingming <ddd>
2023-07-16 19:00:51 +0800

    P55 【vue3+vite重构通用后台】7-header组件中element-plus图标的使用详解

---
+++ src/components/CommonHeader.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="l-content">
+    <!-- <el-icon >
+        <Edit />
+    </el-icon>
+    <Edit style="width: 1em; height: 1em; margin-right: 8px" />
+    <el-icon><ChatDotRound /></el-icon> -->
+    <el-button size="small">
+        <el-icon :size="20">
+            <Menu />
+        </el-icon>
+    </el-button>
+</div>
+<div class="r-content">
+
+</div>
+</template>
--- src/view/Main.vue
+++ src/view/Main.vue
@@ -1,14 +1,24 @@
 <template>
-    <div class="common-layout">
-      <el-container>
+<div class="common-layout">
+    <el-container>
         <el-aside width="200px">Aside</el-aside>
         <el-container>
-          <el-header>Header</el-header>
-          <el-main>
-            <router-view></router-view>
-          </el-main>
+            <el-header>
+                <CommonHeader></CommonHeader>
+            </el-header>
+            <el-main>
+                <router-view></router-view>
+            </el-main>
         </el-container>
-      </el-container>
-    </div>
-  </template>
+    </el-container>
+</div>
+</template>
+
   
+<script>
+import {defineComponent} from 'vue'
+import CommonHeader from '../components/CommonHeader.vue'
+export default defineComponent({ 	
+    components:{CommonHeader} ,
+}) 	
+</script>
